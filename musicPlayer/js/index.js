09b0b60b9e72c398074ea8cc56580b245af0bb35
jinjianjun <13141374484@163.com>
2018-08-29 09:08:52 +0800

    测试1

--- musicPlayer/js/index.js
+++ musicPlayer/js/index.js
@@ -1,9 +1,20 @@
+
+var currentIndex=0
+var audio= new Audio()
+audio.autoplay=true
+
+
+getMusicList(function(list){//将参数传递给这里的function
+    loadMusic(list[currentIndex])//通过改变currentIndex来实现播放第几首歌的功能
+})
+
+function getMusicList(callback){
 var xhr = new XMLHttpRequest()
 xhr.open('GET','music.json',true)
 xhr.onload=function(){
   if((xhr.status>=200&&xhr.status<300)||xhr.status===304){
       console.log(JSON.parse(this.responseText))
-      window.musicList=JSON.parse(this.responseText)
+     callback(JSON.parse(this.responseText))
   }else{
       console.log('获取数据失败')
   }   
@@ -12,3 +23,8 @@ xhr.onerror=function(){
     console.log('网络异常')
 }
 xhr.send()
+}
+function loadMusic(musicObj){
+    console.log('begin play', musicObj)
+    audio.src=musicObj
+}
