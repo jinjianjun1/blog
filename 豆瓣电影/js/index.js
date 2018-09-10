776655f2dd51c51cbaa33da2a6c141179a81eebb
jinjianjun <13141374484@163.com>
2018-09-10 17:57:10 +0800

    first-section

--- 豆瓣电影/js/index.js
+++ 豆瓣电影/js/index.js
@@ -90,6 +90,167 @@
 //       })
 //       return actorArr.join('、')
 //     })
-//     $('.top250').append($node)
+//     return $node
 //   })
 // }
+
+var top250={
+  init:function(){
+  this.$container=$('.firstpage')
+  this.bind()
+  this.start()
+  this.isloading=false
+  this.isFinish=false
+  this.index=0
+  },
+  bind:function(){
+     var _this=this
+    var clock
+    this.$container.on('scroll',function(){//函数节流
+  if(clock){clearTimeout}
+ clock=setTimeout(function() {
+  if(_this.$container.find('.top250').height()-10 <=_this.$container.height+
+  _this.$container.scrollTop()){
+    _this.start()
+   } 
+ }, 300);
+  
+})
+  },
+  start:function(){
+   var _this=this
+   this.getData(function(data){
+     _this.render(data)
+   })
+   
+  },
+  getData:function(callback){
+    var _this=this
+    console.log(_this)
+    if(_this.isloading) return
+    _this.isloading=true
+    _this.$container.find('.isloading').show()
+    $.ajax({
+    url:'http:api.douban.com/v2/movie/top250',
+    type:'GET',
+    data:{
+    start:_this.index||0
+    },
+    dataType:'jsonp',      
+    }).done(function(ret){
+      console.log(ret)
+      _this.index+=20
+      if(_this.index >=ret.total){
+        _this.isFinish=true  
+      }
+      callback&&callback(ret)
+    }).fail(function(){
+      console.log('error to find data')
+    }).always(function(){
+      _this.isloading=false
+      _this.$container.find('.isloading').hide()
+    })
+  
+  },
+  render:function(data){        
+    var _this=this
+    data.subjects.forEach(function(movie){
+      var tpl= 
+      `
+     <div class="item">
+     <a href="#">
+       <div class="cover">
+         <img src="http://img7.doubanio.com/view/photo/s_ratio_poster/public/p480747492.jpg" alt="">
+       </div>
+       <div class="detail">
+         <h2>霸王别姬</h2>
+         <div class="extra">
+           <span class="score">9.3</span> / <span class="collect"></span>收藏        
+         </div>
+         <div class="extra"><span class="year"></span>/  <span class="type"></span> </div>
+         <div class="extra">导演:<span class="director"></span></div>
+         <div class="extra">主演:<span class="actor"></span></div>
+       </div>
+     </a>
+   </div>
+     `
+     var $node=$(tpl)
+     $node.find('.cover img').attr('src',movie.images.medium)
+     //$('section').eq(0).append($node)
+     $node.find('.detail h2').text(movie.title)
+     $node.find('.score').text(movie.rating.average)
+     $node.find('.collect').text(movie.collect_count)
+     $node.find('.year').text(movie.year)
+     $node.find('.type').text(movie.genres.join('/'))
+     $node.find('.director').text(function(){
+       var directorsArr=[]
+       movie.directors.forEach(function(item){
+         directorsArr.push(item.name)
+       })
+       return directorsArr.join('、')
+     })
+     $node.find('.actor').text(function(){
+       var actorArr=[]
+       movie.casts.forEach(function(item){
+         actorArr.push(item.name)
+       })
+       return actorArr.join('、')
+     })
+     _this.$container.find('.top250').append($node)
+    })
+  },
+  isToBottom:function(){
+    return this.$container.find('.top250')<=this.$container.height() + this.$container.scrollTop() +10
+    var clock
+
+
+  }
+
+}
+
+var us={
+  init:function(){
+    console.log('us')
+  },
+  bind:function(){
+
+  },
+  start:function(){
+
+  }
+}
+
+var search={
+  init:function(){
+    console.log('search')
+  },
+  bind:function(){
+
+  },
+  start:function(){
+
+  }
+}
+
+
+
+
+var app= {
+  init: function(){
+    this.$tabs=$('footer>div')
+    this.$panels=$('section')
+    this.bind()
+
+    top250.init()
+    us.init()
+    search.init()
+  },
+  bind:function(){
+    var _this=this
+    this.$tabs.on('click',function(){
+      $(this).addClass('active').siblings().removeClass('active')
+      _this.$panels.eq($(this).index()).fadeIn().siblings().hide()
+    })
+  }
+}
+app.init()
