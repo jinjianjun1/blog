ffe9aa2f259048f73778401985f9a67acec3f859
jason <13141374484@163.com>
2018-09-19 23:18:52 +0800

    1

--- 全屏音乐播放器/js/index.js
+++ 全屏音乐播放器/js/index.js
@@ -20,17 +20,21 @@ var Footer={
   this.render()    
   this.isToEnd =false
   this.isStart =true
+   this.isAnimate=false
   },
   bind:function(){
   var  _this=this
   this.$rightBtn.on('click',function(){
+    if(_this.isAnimate) return
     var itemWidth=_this.$footer.find('li').outerWidth(true)
     var itemCount=Math.floor(_this.$box.width()/itemWidth) 
     
     if(!_this.isToEnd){
+      _this.isAnimate=true
       _this.$ul.animate({
         left: '-='+ itemCount*itemWidth
       },400,function(){
+        _this.isAnimate =false
         _this.isStart=false
         console.log(parseFloat(_this.$ul.css('left')),parseFloat(_this.$ul.width()))
         if(parseFloat(_this.$box.width())-parseFloat(_this.$ul.css('left'))>=parseFloat(_this.$ul.width())){
@@ -43,11 +47,13 @@ var Footer={
   this.$leftBtn.on('click',function(){
     var itemWidth=_this.$footer.find('li').outerWidth(true)
     var itemCount=Math.floor(_this.$box.width()/itemWidth) 
-    
+    if(_this.isAnimate) return
     if(!_this.isStart){
+      _this.isAnimate=true
       _this.$ul.animate({
         left: '+='+ itemCount*itemWidth
       },400,function(){
+        _this.isAnimate=false
         _this.isToEnd=false
         console.log(parseFloat(_this.$ul.css('left')),parseFloat(_this.$ul.width()))
         if(parseFloat(_this.$ul.css('left'))>=0){
@@ -58,7 +64,10 @@ var Footer={
   })
   this.$footer.on('click','li',function(){
     $(this).addClass('active').siblings().removeClass('active')
-    EventConter.fire('select-albumn',$(this).attr('data-channel-id'))
+    EventConter.fire('select-albumn',{
+      channelId:$(this).attr('data-channel-id'),
+      channelName:$(this).attr('data-channel-name')
+    })
   })
   },
   render(){
@@ -73,7 +82,7 @@ var Footer={
   renderFooter:function(channels){
     var html=''
   channels.forEach(function(channel){
-    html +='<li data-channel-id='+channel.channel_id+'>'
+    html +='<li data-channel-id='+channel.channel_id+' data-channel-name='+channel.name+'>'
     +'<div class="cover" style="background-image:url('+channel.cover_small+')"></div>'
     +'<h3>'+channel.name+'<h3>'
     +'</li>'
@@ -91,15 +100,54 @@ var Footer={
 }
 
 
-var App={
+var Fm={
   init:function(){
     this.bind()
+    this.$container=$('#page-music')
+    this.audio= new Audio()
+    this.audio.autoplay=true//这样的话只需修改src就可以播放音乐
+
   },
   bind:function(){
-    EventConter.on('select-albumn',function(e,data){
-      console.log('select',data)
+    var _this=this
+    EventConter.on('select-albumn',function(e,channelObj){
+      _this.channelId=channelObj.channelId
+      _this.channelName=channelObj.channelName
+      _this.loadMusic(function(){
+        _this.setMusic()
+      })
+    }) 
+    //  console.log(this) 
+    //  console.log(this.$container)
+    // this.$container.find('.btn-play').on('click',function(){
+    //  var $btn =$(this)
+    //   if($btn.hasClass('icon-play')){
+    //     $btn.removeClass('icon-play').addClass('icon-pause')
+    //   }else{
+    //     $btn.removeClass('icon-pause').addClass('icon-play')
+    //   }
+    // })
+  },
+  loadMusic(callback){
+    var _this =this
+    $.getJSON('https://jirenguapi.applinzi.com/fm/getSong.php',{channel:this.channelId}).done(function(ret){
+      //console.log(ret.song[0])
+      _this.song=ret['song'][0]
+     // console.log(ret['song'][0])
+      callback()
+      //console.log(this.$container) 
+
     })
+  },
+  setMusic(){
+    console.log(this.$container)
+    this.audio.src=this.song.url
+    $('.bg').css('background-image','url('+this.song.picture+')')
+    this.$container.find('.aside figure').css('background-image','url('+this.song.picture+')')
+    this.$container.find('.detail h1').text(this.song.title)
+    this.$container.find('.detail .author').text(this.song.artist)
+    this.$container.find('.tag').text(this.channelName)
   }
 }
 Footer.init()
-App.init()
+Fm.init()
