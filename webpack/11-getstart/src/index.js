e790405d1dc60f7db1cddd79d6c530273a169fe3
jason <13141374484@163.com>
2018-10-06 00:09:26 +0800

    1

--- webpack/11-getstart/src/index.js
+++ webpack/11-getstart/src/index.js
@@ -1,10 +1,6 @@
-function component() {
-    var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+//let $=require('jquery')
+import $ from 'jquery'
+// $('body').append($('<h1> 试用webpack</h1>'))
+import {lunbo} from './zujian/lunbo'
+
+lunbo.init()
