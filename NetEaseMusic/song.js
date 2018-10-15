2cb82b03539ef10ca474acce01da08aeb0728a37
jason <13141374484@163.com>
2018-10-15 19:57:49 +0800

    将播放界面的歌词完成

--- NetEaseMusic/song.js
+++ NetEaseMusic/song.js
@@ -1,6 +1,6 @@
 $(function(){
  
-    let id =  location.search.match(/\bid=([^&]*)/)[1]//正则的括号加错位置了，血的教训。
+    let id =  location.search.match(/\bid=([^&]*)/)[1]//之前正则的括号加错位置了，血的教训。
 
     $.get('./songs.json').then(function(response){
         let songs=response
@@ -33,9 +33,35 @@ $(function(){
         $('.icon-play').on('click',function(){
             audio.play()
             $('.disc-container').addClass('playing')
-        })         
+        })   
+        
+        setInterval(()=>{
+            
+           let seconds= audio.currentTime
+           let munites=~~(seconds/60)
+           let left=seconds-munites*60
+           let time =`${pad(munites)}:${pad(left)}`
+           let $lines=$('.lines>p')
+           let $whitchLine
+        //    console.log($lines)
+           for(let i=0;i<$lines.length;i++){
+               if($lines.eq(i+1)!==undefined&&$lines.eq(i).attr('data-time')<time&&$lines.eq(i+1).attr('data-time')>time){
+                   $whitchLine=$lines.eq(i)
+                   break
+                }
+           }
+           if($whitchLine){
+               $whitchLine.addClass('active').prev().removeClass('active')
+               let top = $whitchLine.offset().top
+               let linesTop=$('.lines').offset().top
+               let detal=top-linesTop-$('.lyric').height()/3
+               $('.lines').css(`transform`,`translateY(-${detal}px)`)
+           }
+        },300)
     }
-
+    function pad(number){
+        return number>10?number+'' : '0'+number
+    }
     
     function parseLyric(lyric){
         let array=lyric.split('\n')
