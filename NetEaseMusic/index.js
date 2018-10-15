db45b55bab3df0b05d0e84f23741cef476a36d49
jason <13141374484@163.com>
2018-10-15 23:33:55 +0800

    完善了tab切换

--- NetEaseMusic/index.js
+++ NetEaseMusic/index.js
@@ -24,5 +24,33 @@ $(function(){
     },function(){
         throw Error
     })
-    
+    $('.siteNav').on('click','ol.tabItems>li',function(e){
+        // console.log(e)       
+       let $li =$(e.currentTarget).addClass('active')
+       $li.siblings().removeClass('active')
+       let index =$li.index()
+    //    console.log($li,index)
+       $('.tabContent>li').eq(index).addClass('active')
+       .siblings().removeClass('active')
+       $li.trigger('tabChange',index)
+    })
+    $('.siteNav').on('tabChange',function(e,index){
+        let $li=$('.tabContent>li').eq(index)
+        if($li.attr('data-downloaded')==='yes')return
+
+       if(index ===1){
+           $.get('./page2.json').then((response)=>{
+               console.log(response)
+               $li.text(response.content)
+               $li.attr('data-downloaded','yes')
+            })
+       }else if(index===2){
+           $.get('./page3.json').then((response)=>{
+               console.log(response)
+               $li.text(response.content)
+               $li.attr('data-downloaded','yes')
+
+           })
+       }
+    })
 })
